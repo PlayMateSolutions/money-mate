bf62a661eda999c40a70895b01b0371d1f176380
Ramaraj T <jsramraj@gmail.com>
2026-03-22 19:45:38 +0530

    add pwa and dexie configurations

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'money-mate',
+  appId: 'com.moneymate.app',
+  appName: 'MoneyMate',
   webDir: 'www'
 };
 
