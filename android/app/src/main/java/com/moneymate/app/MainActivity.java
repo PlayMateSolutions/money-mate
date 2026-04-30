6a3248d22a4cf37eab1fa9e596b047d41fe78211
Ramaraj T <jsramraj@gmail.com>
2026-04-30 20:54:32 +0530

    trial

--- android/app/src/main/java/com/moneymate/app/MainActivity.java
+++ android/app/src/main/java/com/moneymate/app/MainActivity.java
@@ -3,13 +3,20 @@ package com.moneymate.app;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import ee.forgr.capacitor.social.login.SocialLoginPlugin;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 
-public class MainActivity extends BridgeActivity {
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Register the SocialLogin plugin for OAuth scopes
         registerPlugin(SocialLoginPlugin.class);
+    }
+
+    @Override
+    public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {
+        // This method indicates to the plugin that MainActivity has been properly configured
+        // for OAuth scopes usage. Required by @capgo/capacitor-social-login plugin.
     }
 }
