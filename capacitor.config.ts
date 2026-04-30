1e6e6071d6067952093c71de1a79c24f3f5c1e32
Ramaraj T <jsramraj@gmail.com>
2026-04-30 23:26:03 +0530

    fix the apk build error (#37)
    * update android configurations
    
    * update social login configuration
    
    * fix the package name
    
    * trial
    
    * update android config
    
    * update the keystore name
    
    * add more logs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.moneymate.app',
   appName: 'MoneyMate',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SocialLogin: {
+      google: {
+        androidClientId: "1031239235658-a21arqinflilmv504uqdge4pv2debf27.apps.googleusercontent.com"
+      }
+    }
+  }
 };
 
 export default config;
