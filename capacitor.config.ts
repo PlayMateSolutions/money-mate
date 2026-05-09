8ebe10bf22da7019c6b6a9495a8793f4ca13810f
Ramaraj T <jsramraj@gmail.com>
2026-05-09 17:15:28 +0530

    update the google client id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,7 +8,7 @@ const config: CapacitorConfig = {
   plugins: {
     SocialLogin: {
       google: {
-        androidClientId: "102904530835-dt70ccodl3smiula3frcbrt5ubf8out6.apps.googleusercontent.com"
+        androidClientId: "102904530835-vrtlhimba7aqdaqcm56orbvj19n3ilhi.apps.googleusercontent.com"
       }
     },
     SplashScreen: {
